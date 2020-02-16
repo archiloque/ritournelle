c48b0ddbc063f816f3142e887498bc2913df1b8f
Julien Kirch <code@archiloque.net>
2020-02-16 22:28:47 +0100

    no message

--- test/cases/simple_interface/output.rit.rb
+++ test/cases/simple_interface/output.rit.rb
@@ -11,7 +11,29 @@
 #   end
 
 
-class MyClass
+class MyClass1
+  # @!parse
+  #   include MyInterface
+
+  # @param [Integer] constructor_index
+  def initialize(constructor_index, *parameters)
+    send("initialize—#{constructor_index}", *parameters)
+  end
+  
+  # @param [Ritournelle::Runtime::StdLib::Int] member2
+  def initialize—0(member2)
+    @member2 = member2
+  end
+  
+  # @return [Ritournelle::Runtime::StdLib::Int]
+  # @note Declared name is member2_plus2
+  def member2_plus2—0()
+    return @member2.plus_Int(Ritournelle::Runtime::StdLib::Int.new(2))
+  end
+end
+
+
+class MyClass2
   # @!parse
   #   include MyInterface
 
@@ -33,6 +55,10 @@ class MyClass
 end
 
 # @type [MyInterface]
-m =
-  MyClass.new(0, Ritournelle::Runtime::StdLib::Int.new(2))
-m.member2_plus2—0()
+m1 =
+  MyClass1.new(0, Ritournelle::Runtime::StdLib::Int.new(2))
+m1.member2_plus2—0()
+# @type [MyInterface]
+m2 =
+  MyClass2.new(0, Ritournelle::Runtime::StdLib::Int.new(2))
+m2.member2_plus2—0()
